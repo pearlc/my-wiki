22be096327ece3aaced3ae8adc10bfd80ebe2069
pearlc <rchung@kr-rchung1mm1.local>
2014-05-02 09:21:46 +0900

    bootstrap, 테스트 에셋들 추가

---
+++ app/assets/js/lib.js
@@ -0,0 +1,9 @@
+/**
+ * Created by rchung on 2014. 5. 1..
+ */
+
+console.log('This is lib.js');
+
+var myfunction = function(parameterA, parameterB) {
+    return parseInt(parameterA, 10) + parseInt(parameterB, 10);
+};
---
+++ app/assets/js/main.js
@@ -0,0 +1,10 @@
+/**
+ * Created by rchung on 2014. 4. 20..
+ */
+
+console.log('This is main.js');
+
+
+(function() {
+    myname = 'dfdf';
+})();

926083ae3009132ac4218553f7077d15bcbc8fbe
pearlc <rchung@kr-rchung1mm1.local>
2014-05-04 17:17:50 +0900

    db migration 에 nick_name 필드 추가

---
+++ app/database/migrations/2014_05_04_061805_add_nick_name_to_user_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddNickNameToUserTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('users', function(Blueprint $table)
+		{
+			//
+            $table->string('nick_name')->unique()->after('last_name');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('users', function(Blueprint $table)
+		{
+			//
+            $table->dropColumn('nick_name');
+        });
+	}
+
+}

b99193e59cb676ee71c9b274b6031652f7d9b4ad
pearlc <rchung@kr-rchung1mm1.local>
2014-05-05 17:26:56 +0900

    user route 그룹에 로그인/비로그인 필터 적용

--- app/routes.php
+++ app/routes.php
@@ -19,6 +19,19 @@
 
 // TODO : 라우팅 실패시 404 페이지로 리디렉트 되도록
 
+Route::filter('sentryAuth', function() {
+        if (!Sentry::getUser()) {
+            return Redirect::route('index');
+        }
+    });
+
+Route::filter('sentryNotAuth', function() {
+
+        if (Sentry::getUser()) {
+            return Redirect::route('index');
+        }
+    });
+
 //Route::get('/', function() { return View::make('hello'); });
 //
 Route::get('/', array('as' => 'index', 'uses' => 'MainController@index'));
@@ -27,28 +40,36 @@ Route::get('/markuptest', array('as' => 'markuptest', 'uses' => 'MarkupTestContr
 Route::get('/markuptest/email', array('as' => 'markuptest_email', 'uses' => 'MarkupTestController@sendEmail'));
 Route::get('/markuptest/client_info', array('as' => 'markuptest_client_info', 'uses' => 'MarkupTestController@clientInfo'));
 
+// User
 Route::group(array('prefix' => 'user'), function() {
 
         // TODO : register, login, reset password(reset code 왔다갔다 하는곳) 에 https 적용 (걍 user 그룹엔 다 적용하는것 고려)
 
-        Route::get('/', array('as' => 'user', function() { return Redirect::route('user_profile');}));
-        Route::get('/profile', array('as' => 'user_profile', 'uses' => 'UserController@profile'));
-        Route::get('/profile/edit', array('as' => 'user_profile_edit', 'uses' => 'UserController@profileEdit'));
-        Route::get('/welcome', array('as' => 'user_welcome', 'uses' => 'UserController@welcome'));
-        Route::get('/register', array('as' => 'user_register', 'uses' => 'UserController@register'));
-        Route::get('/activate/{activationCode}', array('as' => 'user_activate', 'uses' => 'UserController@activate'));
-        Route::get('/login', array('as' => 'user_login', 'uses' => 'UserController@login'));
-        Route::get('/logout', array('as' => 'user_logout', 'uses' => 'UserController@logout'));
-        Route::get('/delete', array('as' => 'user_delete', 'uses' => 'UserController@delete'));
-        Route::get('/delete_confirm', array('as' => 'user_delete_confirm', 'uses' => 'UserController@deleteConfirm'));
-        Route::get('/forgot_password', array('as' => 'user_forgot_password', 'uses' => 'UserController@forgotPassword'));
-        Route::get('/password_reset/{payload}', array('as' => 'user_password_reset', 'uses' => 'UserController@passwordReset'));
-        Route::get('/password_edit', array('as' => 'user_password_edit', 'uses' => 'UserController@passwordEdit'));
+        Route::group(array('before' => 'sentryAuth'), function() {
+                Route::get('/', array('as' => 'user', function() { return Redirect::route('user_profile');}));
+                Route::get('/profile', array('as' => 'user_profile', 'uses' => 'UserController@profile'));
+                Route::get('/profile/edit', array('as' => 'user_profile_edit', 'uses' => 'UserController@profileEdit'));
+                Route::get('/logout', array('as' => 'user_logout', 'uses' => 'UserController@logout'));
+                Route::get('/delete', array('as' => 'user_delete', 'uses' => 'UserController@delete'));
+                Route::get('/delete_confirm', array('as' => 'user_delete_confirm', 'uses' => 'UserController@deleteConfirm'));
+                Route::get('/password_edit', array('as' => 'user_password_edit', 'uses' => 'UserController@passwordEdit'));
 
-        Route::post('/login', array('as' => 'user_login_post', 'uses' => 'UserController@loginPost'));
-        Route::post('/register', array( /* 'before' => 'csrf', */'as' => 'user_register_post', 'uses' => 'UserController@registerPost'));
-        Route::post('/forgot_password', array('as' => 'user_forgot_password_post', 'uses' => 'UserController@forgotPasswordPost'));
-        Route::post('/password_reset', array('as' => 'user_password_reset_post', 'uses' => 'UserController@passwordResetPost'));
-        Route::post('/password_edit', array('as' => 'user_password_edit_post', 'uses' => 'UserController@passwordEditPost'));
-        Route::post('/profile/edit', array('as' => 'user_profile_edit_post', 'uses' => 'UserController@profileEditPost'));
+                Route::post('/profile/edit', array('as' => 'user_profile_edit_post', 'uses' => 'UserController@profileEditPost'));
+                Route::post('/password_edit', array('as' => 'user_password_edit_post', 'uses' => 'UserController@passwordEditPost'));
+            });
+
+        Route::group(array('before' => 'sentryNotAuth'), function() {
+                Route::get('/register', array('as' => 'user_register', 'uses' => 'UserController@register'));
+                Route::get('/welcome', array('as' => 'user_welcome', 'uses' => 'UserController@welcome'));
+                Route::get('/activate/{activationCode}', array('as' => 'user_activate', 'uses' => 'UserController@activate'));
+                Route::get('/login', array('as' => 'user_login', 'uses' => 'UserController@login'));
+                Route::get('/forgot_password', array('as' => 'user_forgot_password', 'uses' => 'UserController@forgotPassword'));
+                Route::get('/password_reset/{payload}', array('as' => 'user_password_reset', 'uses' => 'UserController@passwordReset'));
+
+
+                Route::post('/register', array( /* 'before' => 'csrf', */'as' => 'user_register_post', 'uses' => 'UserController@registerPost'));
+                Route::post('/login', array('as' => 'user_login_post', 'uses' => 'UserController@loginPost'));
+                Route::post('/forgot_password', array('as' => 'user_forgot_password_post', 'uses' => 'UserController@forgotPasswordPost'));
+                Route::post('/password_reset', array('as' => 'user_password_reset_post', 'uses' => 'UserController@passwordResetPost'));
+            });
     });

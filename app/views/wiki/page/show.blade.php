f30a84037b115ceacdd9d52361247fbf7c8d7dd2
pearlc <rchung@kr-rchung1mm1.local>
2014-06-01 14:03:55 +0900

    수정된 날짜 span 생성

--- app/views/wiki/page/show.blade.php
+++ app/views/wiki/page/show.blade.php
@@ -4,7 +4,11 @@
 
 <h1>{{{ $page['title'] }}}</h1>
 
-<span>{{ link_to_route('wiki.page.edit', '수정하기', ['title' => $page['title']]) }}</span>
+<p>
+    <span>{{ '최종 수정 : ' . $page['updated_at']->diffForHumans(\Carbon\Carbon::now()) }} </span>
+    /
+    <span>{{ link_to_route('wiki.page.edit', '수정하기', ['title' => $page['title']]) }}</span>
+</p>
 
 <p>
     {{ $revision['text'] }}
